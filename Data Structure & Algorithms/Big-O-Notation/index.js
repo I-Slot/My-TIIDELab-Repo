d78d7082624453a99b16ed17215c64196431c058
github_i-slot <ismailadesholaharun@gmail.com>
2023-07-18 11:41:17 +0100

    update

--- Data Structure & Algorithms/Big-O-Notation/index.js
+++ Data Structure & Algorithms/Big-O-Notation/index.js
@@ -1,31 +1,32 @@
-// const nemo = ["nemo"];
-// let t0 = performance.now();
-// console.log(t0);
-// function findNemo(arrays) {
-//   for (let i = 0; i < arrays.length; i++) {
-//     if (arrays[i] === "nemo") {
-//       console.log("Nemo is found.");
-//     }
-//   }
-//   let t1 = performance.now();
-//   console.log(t1);
-//   console.log(`call to find nemo ${t1 - t0}`);
-// }
-// findNemo(nemo);
+const nemo = ["nemo"];
+let t0 = performance.now();
+console.log(t0);
+function findNemo(arrays) {
+  for (let i = 0; i < arrays.length; i++) {
+    if (arrays[i] === "nemo") {
+      console.log("Nemo is found.");
+    }
+  }
+  let t1 = performance.now();
+  console.log(t1);
+  console.log(`call to find nemo ${t1 - t0}`);
+}
+findNemo(nemo);
 
 alert("connected");
 
-const arr = ["isamil"];
+// const arr = ["isamil"];
 
-let time1 = performance.now();
-function arrTT() {
-  for (let i = 0; i < arr.length; i++) {
-    console.log();
-  }
+// let time1 = performance.now();
+// console.log()
+// function arrTT() {
+//   for (let i = 0; i < arr.length; i++) {
+//     console.log();
+//   }
 
-  let time2 = performance.now();
+//   let time2 = performance.now();
 
-  console.log(`our time complexity is ${time2 - time1}`);
-}
+//   console.log(`our time complexity is ${time2 - time1}`);
+// }
 
-arrTT();
+// arrTT();
