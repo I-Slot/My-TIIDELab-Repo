0ec6c3477e3bceea74f6684a61c23bf356cd1cb5
github_i-slot <ismailadesholaharun@gmail.com>
2023-08-14 11:31:14 +0100

    update

--- Javascript/index.js
+++ Javascript/index.js
@@ -21,3 +21,21 @@ console.log(firstName);
 // The console is a debugging tool for developers
 // The console are of 2 types (1)Terminal console And (2) Browser console
 // The terminal console is used when javascript code is run in the node environment
+// A website can be hacked through the browser console.
+
+/*
+Let : When let is used a variable can't be re-declared but only reassign.
+ */
+/*Variable naming convention
+1- Variables name can't start with a number e.g 23House xxx
+2- Variables must be space free
+
+Variable Naming Styles(Contention)
+1- Camel Case: e.g ismailAdesholaOgundele
+2- Snake Case: ismail_adeshola_ogundele
+3- Pascal case: IsmailAdesholaOhundele
+
+*The standard naming convention in Javascript is Camel Case.
+In PHP : snake case is the standard
+
+*/
