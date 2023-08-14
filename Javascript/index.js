393723587cdca06bf205bccd3fd4848e00085d7f
github_i-slot <ismailadesholaharun@gmail.com>
2023-08-14 11:15:16 +0100

    update

---
+++ Javascript/index.js
@@ -0,0 +1,23 @@
+// Js Fundamental
+// Introduction To Javascript
+
+// Variables
+// Anything that is not constant, which means it has dynamic content(Value) most times.
+/* Variable: Is a named container, used to save named value.
+// When declaring variables we use 2 key words
+ (1) let 
+ (2) const 
+ and the old way is by (var)*/
+// Before ECMA Variable can be written as
+// name = "Ismail"
+
+/*Let key word */
+
+// Expression
+// Statement : A statement should be terminated when ever is declared.
+
+let firstName = "Ismail";
+console.log(firstName);
+// The console is a debugging tool for developers
+// The console are of 2 types (1)Terminal console And (2) Browser console
+// The terminal console is used when javascript code is run in the node environment
