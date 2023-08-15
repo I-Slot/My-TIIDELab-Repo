9350c063d1aa4ad23e84364a84c32bc6ca09ed77
github_i-slot <ismailadesholaharun@gmail.com>
2023-08-15 13:50:20 +0100

    update

--- Javascript/index.js
+++ Javascript/index.js
@@ -29,6 +29,8 @@ Let : When let is used a variable can't be re-declared but only reassign.
 /*Variable naming convention
 1- Variables name can't start with a number e.g 23House xxx
 2- Variables must be space free
+3- special characters can't start variables except ( _ and $)
+4- Javascript reserved words (if, else, switch etc.)
 
 Variable Naming Styles(Contention)
 1- Camel Case: e.g ismailAdesholaOgundele
@@ -39,3 +41,106 @@ Variable Naming Styles(Contention)
 In PHP : snake case is the standard
 
 */
+
+/*
+2. const 
+const should be used for something that won't change
+const variable can't be re-declared and re assign.
+ */
+
+/*
+/////////////
+DAY 2
+/////////////
+ */
+
+// Data Types
+/*
+1.Number e.g 62772, 83825
+2.String e.g Ismail, Anas
+3.Boolean (Yes/No , True/False)
+4.Null 
+5.Undefined
+6.Object : syntax : {key : value}
+ e.g let Ismail = {
+    firstName: "Ismail",
+    age: 99,
+    nationality: "Nigerian"
+ }
+ 7.Array 
+ e.g array = [1,2,3,4,5,6,7,8,9]
+ */
+
+console.log(Number.MAX_SAFE_INTEGER);
+
+let age = null;
+console.log(age);
+
+let year;
+console.log(year);
+
+let something = "";
+console.log(something.length);
+
+// Object example
+
+let fellow = {
+  firstName: "Ismail",
+  age: 99,
+  educationLevel: "PhD",
+  complexion: "black",
+  gender: "male",
+};
+
+console.log(fellow.educationLevel);
+
+let me = null;
+let number = 4;
+
+let und = undefined;
+
+let result = me * number;
+console.log(result);
+console.log(typeof me);
+
+//Operators Unary operators && Binary Operator
+/*
+Unary Operators : Need only one operand e.g typeof (A unary operation is an operation with only one operand)
+binary Operators : Needs 2 operand (+, -, *)
+
+Arithmetic
+// + - * / %
+
+logical operators
+// AND OR NOT
+AND &&
+OR ||
+NOT !=
+
+// COMPARISON
+ GREATER THAN >
+ LESS THAN <
+ GREATER OR EQUAL >=
+ LESS OR EQUAL <=
+ EQUAL : ==
+ EXACTLY EQUAL : ===
+
+ ASSIGNMENT OPERATOR (=)
+ */
+
+// practice
+
+let profession = "Engineer";
+
+// Concatenation
+
+// Template literals
+
+//User Interface
+
+// prompt, alert, confirm
+
+let userName = prompt("Please what your name?");
+let exp = `Welcome ${userName}!`;
+let exps = exp;
+alert(exps);
