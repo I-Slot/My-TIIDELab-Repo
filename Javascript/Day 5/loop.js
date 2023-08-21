4d1864add005ea62e33f9d6c83a6852c4955231f
github_i-slot <ismailadesholaharun@gmail.com>
2023-08-21 10:31:22 +0100

    update

---
+++ Javascript/Day 5/loop.js
@@ -0,0 +1,65 @@
+let i = 8;
+// i = i + 1;
+
+// i += 1;
+i++;
+// console.log(i);
+/*
+1. while loop
+2. do while loop
+3. for loop
+ */
+
+//while loop
+
+/*
+let n = 'initial value'
+while(condition)
+{
+  action
+}
+*/
+
+// while Loop example
+
+let x = 10;
+while (x <= 30) {
+  //   console.log(x);
+  x++;
+}
+
+// Example 2 for loop
+
+// let y = 1;
+// while (y <= 12) {
+//   r = y * 5;
+
+//   console.log(`5 X ${y} = ${r}`);
+//   y++;
+// }
+
+let m = 1;
+while (m <= 12) {
+  let multiple = m * 8;
+  //   console.log(`8 X ${m} = ${multiple}`);
+  m++;
+}
+
+// Do while loop
+/*
+syntax
+
+do {
+
+}
+while(condition)
+ */
+
+// example of do while loop
+
+let b = 19;
+do {
+  b *= 2;
+  console.log("");
+  b;
+} while (b <= 25);
